d787c4eb70f44488d247444b1109a9462aa8a542
JeeyaahVaswani <91950574+JeeyaahVaswani@users.noreply.github.com>
2023-10-18 18:33:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,26 +16,17 @@ function draw(){
 }
 var previous_result='';
 
-function gotResult(error,results)
-{
-  if(error){
-    console.error(error);
-  } else{
-    if((results[0].confidence> 0.5)&&(previous_result !=results[0].label)){
-      console.log(results);
-      previous_result=results[0].label;
-      var synth=window.speechSynthesis;
-      speak_data='Object detected is - '+results[0].label;
-      var utterThis= new SpeechSynthesisUtterance(speak_data);
-      synth.speak(utterThis);
-
-      document.getElementById("results_obejct_name").innerHTML=results[0].label;
-      document.getElementById("results_obejct_accuracy").innerHTML=results[0].confidence.toFixed(3);
-
-    }
-  
-  }
-}
+function gotResult(error, results)
+ { if (error) { console.error(error); }
+  else { if((results[0].confidence > 0.5) && (previous_result != results[0].label))
+    { console.log(results); 
+      previous_result = results[0].label; 
+      var synth = window.speechSynthesis; speak_data = 'Object detected is - '+results[0].label;
+       var utterThis = new SpeechSynthesisUtterance(speak_data); 
+       synth.speak(utterThis); 
+       document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+       } } }
 
 
 
